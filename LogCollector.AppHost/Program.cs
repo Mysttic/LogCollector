1307f887a014b6b8edc5e94ef432e977378ca3a5
Jakub Kamiński <jakub.kaminski@intelimedical.pl>
2024-07-26 10:02:46 +0200

    Add LogCollector.Browser.Backend and refactor entities

--- LogCollector.AppHost/Program.cs
+++ LogCollector.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.LogCollector_Service>("logcollector-service");
 
+builder.AddProject<Projects.LogCollector_Browser_Backend>("logcollector-browser-backend");
+
 builder.Build().Run();
